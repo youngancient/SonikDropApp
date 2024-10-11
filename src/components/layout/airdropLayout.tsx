2608165c8eaf3e70b0fb4fa1ec2178539aaa0cca
Victor <ukokjnr@gmail.com>
2024-10-11 11:13:05 +0100

    Fix: Background, formatUint, checking if an address is valid.

--- src/components/layout/airdropLayout.tsx
+++ src/components/layout/airdropLayout.tsx
@@ -5,7 +5,7 @@ import { StepComponent } from "../stepComponent";
 export default function AirdropLayout(props: any) {
 
     return (
-        <div style={{background: "linear-gradient(90deg, #0F195B 0%, #233BD7 100%)"}} className="h-screen overflow-auto">
+        <div style={{backgroundColor: "#0F195B"}} className="h-screen overflow-auto">
             <HeaderComponent />
             <StepComponent />
             {props.children}
