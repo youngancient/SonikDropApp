b5a4d6f13e33933f78bca18606fd27743b08dd0a
Victor <ukokjnr@gmail.com>
2024-11-04 13:01:37 +0100

    Fix: Footer.

--- src/components/footerComponent.tsx
+++ src/components/footerComponent.tsx
@@ -1,14 +1,13 @@
-import { FaFacebook, FaTwitter } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export function FooterComponent() {
     return (
-        <div className="flex justify-between text-white h-[100px] items-center px-2 md:px-[200px]">
-            <div>Footer</div>
-            <div>Footer</div>
-            <div className="flex gap-4">
-                <Link to=""><FaFacebook className="text-[20px]" /></Link>
-                <Link to=""><FaTwitter className="text-[20px]" /></Link>
+        <div className="flex justify-center text-white h-[100px] items-center px-2 md:px-[200px]">
+
+            <div className="flex gap-2 items-center">
+                <div>Contribute to SonikDrop</div>
+                <Link to="https://github.com/youngancient/SonikDropApp"><FaGithub className="text-[20px]" /></Link>
             </div>
         </div>
     );
