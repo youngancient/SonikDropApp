f7d3e0e0e268ad20da89c100f2f59fbc025e74f2
youngancient <tochyokoye@gmail.com>
2024-11-13 23:42:24 +0100

    removed basic supported chains component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import "./App.css";
 import "./connection.ts";
 import { HeaderComponent } from "./components/headerComponent.tsx";
-// import { Link } from "react-router-dom";
-// import { useAppKitState } from "@reown/appkit/react";
-// import { supportedNetworksDetails } from "./constants/chains.ts";
-// import { extractChainId } from "./utils/helpers.ts";
+
 import { Hero } from "./components/heroComponent.tsx";
-//import { SupportedChains } from "./components/supportedChains.tsx";
 
 function App() {
   // const { selectedNetworkId } = useAppKitState();
@@ -15,24 +11,10 @@ function App() {
     <>
       <HeaderComponent showBackButton={false} />
       <Hero />
-      {/* <SupportedChains /> */}
+      
     </>
   );
 }
 
 export default App;
 
-{/* <div className="flex justify-center flex-col items-center gap-4">
-        <h1>SonikDrop</h1>
-        <div className="card" >
-          <Link to="/prepare" className="border-2 border-white/[0.2] px-4 py-1 rounded-lg">Prepare</Link>
-        </div>
-        <p className=" text-[25px]">
-          You're on network:{" "}
-          {selectedNetworkId
-            ? supportedNetworksDetails[
-                parseInt(extractChainId(selectedNetworkId))
-              ]
-            : "Unknown"}
-        </p>
-      </div> */}
