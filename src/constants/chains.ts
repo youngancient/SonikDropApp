00c912db6f1335211b979350874307d3815d54a5
youngancient <tochyokoye@gmail.com>
2024-11-13 21:23:51 +0100

    corrected wrong import

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -28,15 +28,15 @@ export const supportedNetworksDetails:INetworkDetail = {
 
 
 export const ethSettings = {
-  apiKey: process.env.VITE_ALCHEMY_API_KEY,
+  apiKey: import.meta.env.VITE_ALCHEMY_API_KEY,
   network: Network.ETH_SEPOLIA,
 };
 export const baseSettings = {
-  apiKey: process.env.VITE_ALCHEMY_API_KEY,
+  apiKey: import.meta.env.VITE_ALCHEMY_API_KEY,
   network: Network.BASE_SEPOLIA,
 };
 
 export const bnbSettings = {
-  apiKey: process.env.VITE_ALCHEMY_API_KEY,
+  apiKey: import.meta.env.VITE_ALCHEMY_API_KEY,
   network: Network.BNB_TESTNET,
 };
