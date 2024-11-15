e08784e9fb3f5b0243b8d4be5cc98d5d139ec522
youngancient <tochyokoye@gmail.com>
2024-11-15 05:45:15 +0100

    added animation to desktop

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./connection.ts";
 import { HeaderComponent } from "./components/headerComponent.tsx";
 
 import { Hero } from "./components/heroComponent.tsx";
+import { BNB, ETH , Optimism, Solana, Arbitrum, Polygon} from "./components/icons.tsx";
 
 function App() {
   // const { selectedNetworkId } = useAppKitState();
@@ -11,7 +12,12 @@ function App() {
     <>
       <HeaderComponent showBackButton={false} />
       <Hero />
-      
+      <BNB />
+      <ETH />
+      <Optimism />
+      <Solana />
+      <Arbitrum />
+      <Polygon />
     </>
   );
 }
