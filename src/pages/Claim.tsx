fe7fa88f54eba751215cc056a56500b03c1d771d
youngancient <tochyokoye@gmail.com>
2024-12-21 07:40:50 +0100

    working on poap page

--- src/pages/Claim.tsx
+++ src/pages/Claim.tsx
@@ -4,19 +4,23 @@ import { HeaderComponent } from "../components/headerComponent";
 import { ClaimPageStyle, DropListStyle } from "../components/styles/claimpage";
 import { motion } from "framer-motion";
 import { CircleCancel, MagnifyingGlass } from "../components/icons";
-import { DropComp, IDropComp } from "../components/claimComponent";
+import {
+  DropComp,
+  IDropComp,
+  POAPDropComp,
+} from "../components/claimComponent";
 
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 interface TabSwitch {
-  name: string;
+  name: "Tokens" | "POAPs";
   isSelected: boolean;
 }
 const tabs: TabSwitch[] = [
   { name: "Tokens", isSelected: true },
   { name: "POAPs", isSelected: false },
 ];
-const Drops: IDropComp[] = [
+const TokenDrops: IDropComp[] = [
   {
     name: "Pengu",
     creator: "0x0f09D1Fb501041E32170b1B759f1b2ef6349C490",
@@ -91,11 +95,90 @@ const Drops: IDropComp[] = [
   },
 ];
 
+const POAPDrops: IDropComp[] = [
+  {
+    name: "DevconSEA",
+    creator: "0x0f09D1Fb501041E32170b1B759f1b2ef6349C490",
+    date: "24.12.24",
+    totalRewardPool: 2000,
+    totalRewardClaimed: 1500,
+    totalParticipants: 2000,
+  },
+  {
+    name: "Scroll Buidl",
+    creator: "0x0f09D1Fb501041E32170b1B759f1b2ef6349C490",
+    date: "24.10.24",
+    totalRewardPool: 6000,
+    totalRewardClaimed: 5000,
+    totalParticipants: 6000,
+  },
+  {
+    name: "Align Buidl",
+    creator: "0x53182725595443ba2bB9EbEfE716EE72761a3CD3",
+    date: "24.10.24",
+    totalRewardPool: 500,
+    totalRewardClaimed: 250,
+    totalParticipants: 500,
+  },
+  {
+    name: "Build Guild",
+    creator: "0x53182725595443ba2bB9EbEfE716EE72761a3CD3",
+    date: "24.10.24",
+    totalRewardPool: 64000,
+    totalRewardClaimed: 1000,
+    totalParticipants: 64000,
+  },
+  {
+    name: "ChillGuys Meetup",
+    creator: "0x53182725595443ba2bB9EbEfE716EE72761a3CD3",
+    date: "24.11.24",
+    totalRewardPool: 6000,
+    totalRewardClaimed: 1000,
+    totalParticipants: 6000,
+  },
+  {
+    name: "Kaia Club",
+    creator: "0x0f09D1Fb501041E32170b1B759f1b2ef6349C490",
+    date: "24.11.24",
+    totalRewardPool: 150,
+    totalRewardClaimed: 150,
+    totalParticipants: 150,
+  },
+  {
+    name: "Base Meetup",
+    creator: "0x0f09D1Fb501041E32170b1B759f1b2ef6349C490",
+    date: "28.11.24",
+    totalRewardPool: 1800,
+    totalRewardClaimed: 1000,
+    totalParticipants: 1800,
+  },
+  {
+    name: "Bored Devs",
+    creator: "0x0f09D1Fb501041E32170b1B759f1b2ef6349C490",
+    date: "28.11.24",
+    totalRewardPool: 800,
+    totalRewardClaimed: 400,
+    totalParticipants: 800,
+  },
+  {
+    name: "Santa Comes",
+    creator: "0x0f09D1Fb501041E32170b1B759f1b2ef6349C490",
+    date: "28.11.24",
+    totalRewardPool: 1600,
+    totalRewardClaimed: 1500,
+    totalParticipants: 1600,
+  },
+];
+
 const ClaimPage = () => {
   const [stateTabs, setStateTabs] = useState(tabs);
-  const handleTabSwitch = (tab: string) => {
+
+  const [selectedTabName, setSelectedTabName] = useState("Tokens");
+
+  const handleTabSwitch = (tabName: string) => {
+    setSelectedTabName(tabName);
     const newTabs = stateTabs.map((ele) => {
-      return { ...ele, isSelected: ele.name === tab };
+      return { ...ele, isSelected: ele.name === tabName };
     });
     setStateTabs(newTabs);
   };
@@ -157,12 +240,11 @@ const ClaimPage = () => {
           </div>
         </div>
         <DropListStyle className="drop-list mt-[2rem] md:mt-[3rem]">
-          {Drops.map((drop) => (
-            <DropComp
-              key={uuidv4()}
-              {...drop}
-            />
-          ))}
+          {selectedTabName == "Tokens" &&
+            TokenDrops.map((drop) => <DropComp key={uuidv4()} {...drop} />)}
+
+          {selectedTabName == "POAPs" &&
+            POAPDrops.map((drop) => <POAPDropComp key={uuidv4()} {...drop} />)}
         </DropListStyle>
       </ClaimPageStyle>
       <FooterComponent />
