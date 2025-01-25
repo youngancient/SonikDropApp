9393baa8fbea1f93784007e60c9a3154546e6a57
Raji Mustapha <rajimustapha30@gmail.com>
2025-01-25 13:50:14 +0100

    chore: resolved issue 102

--- src/store/slices/settingsSlice.ts
+++ src/store/slices/settingsSlice.ts
@@ -4,6 +4,8 @@ import type { RootState } from "../store";
 
 // Define a type for the slice state
 interface settingsState {
+  airDropName: string;
+  airDropNameError: string;
   nftAddress: string;
   nftAddressError: string;
   claimButtonDeactivated: boolean;
@@ -15,12 +17,15 @@ interface settingsState {
 
 // Define the initial state using that type
 const initialState: settingsState = {
+  airDropName: "",
+  airDropNameError: "",
   nftAddress: "",
   nftAddressError: "",
   claimButtonDeactivated: false,
   airdropStart: "",
   airdropEndMin: "",
   airdropEnd: "",
+
   onlyNFTOwnersCanClaim: false,
 };
 
@@ -30,6 +35,12 @@ export const settingsSlice = createSlice({
   initialState,
   reducers: {
     // Use the PayloadAction type to declare the contents of `action.payload`
+    setAirdropName: (state, action: PayloadAction<string>) => {
+      state.airDropName = action.payload;
+    },
+    setAirdropNameError: (state, action: PayloadAction<string>) => {
+      state.airDropNameError = action.payload;
+    },
     setNftAddress: (state, action: PayloadAction<string>) => {
       state.nftAddress = action.payload;
     },
@@ -50,7 +61,7 @@ export const settingsSlice = createSlice({
     },
     setOnlyNFTOwnersCanClaim: (state, action: PayloadAction<boolean>) => {
       state.onlyNFTOwnersCanClaim = action.payload;
-    }
+    },
   },
 });
 
@@ -61,12 +72,17 @@ export const {
   setClaimButtonDeactivated,
   setNftAddress,
   setOnlyNFTOwnersCanClaim,
-  setAirdropEndMin
+  setAirdropEndMin,
+  setAirdropName,
+  setAirdropNameError,
 } = settingsSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
-export const selectNftAddress = (state: RootState) =>
-  state.settings.nftAddress;
+export const selectAirDropName = (state: RootState) =>
+  state.settings.airDropName;
+export const selectAirDropNameError = (state: RootState) =>
+  state.settings.airDropNameError;
+export const selectNftAddress = (state: RootState) => state.settings.nftAddress;
 export const selectNftAddressError = (state: RootState) =>
   state.settings.nftAddressError;
 export const selectClaimButtonDeactivated = (state: RootState) =>
@@ -75,8 +91,7 @@ export const selectAirdropStart = (state: RootState) =>
   state.settings.airdropStart;
 export const selectAirdropEndMin = (state: RootState) =>
   state.settings.airdropEndMin;
-export const selectAirdropEnd = (state: RootState) =>
-  state.settings.airdropEnd;
+export const selectAirdropEnd = (state: RootState) => state.settings.airdropEnd;
 export const selectOnlyNFTOwnersCanClaim = (state: RootState) =>
   state.settings.onlyNFTOwnersCanClaim;
 
