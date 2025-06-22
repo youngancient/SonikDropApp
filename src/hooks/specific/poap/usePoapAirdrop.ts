7f41c221736282944427ce186f8a84aae2d56031
youngancient <tochyokoye@gmail.com>
2025-06-22 22:23:51 +0100

    Complete Saving and retrieving Merkle proofs on the Server

--- src/hooks/specific/poap/usePoapAirdrop.ts
+++ src/hooks/specific/poap/usePoapAirdrop.ts
@@ -19,16 +19,11 @@ export const usePoapDropFunctions = (poapContractAddress: string) => {
     }
 
     // @note get this merkleproof from the backend server (using the contract address and user address)
-    const merkleProof = [
-      "0x708e7cb9a75ffb24191120fba1c3001faa9078147150c6f2747569edbadee751",
-      "0x8cdd6608c14a222369d97956b504f94500a33c673fd156f8f2da7f980260c91c",
-      "0x79ec436321e0ee4d3657f6b4c44573e6af12266adc6fdb29f3f7de915ce4975d",
-    ];
 
     try {
       setIsMinting(true);
       console.log("got here", userPoapClaim);
-      const tx = await poapDropContract["claimAirdrop(bytes32[])"](merkleProof);
+      const tx = await poapDropContract["claimAirdrop(bytes32[])"](userPoapClaim);
 
       const reciept = await tx.wait();
       return { success: reciept.status === 1, transactionHash: tx.hash };
